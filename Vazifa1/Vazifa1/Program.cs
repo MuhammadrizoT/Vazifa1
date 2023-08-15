2fcc7854b6a727af55fecaac2e3a93ccd0ef2c61
TMuhammadrizo <142306600+TMuhammadrizo@users.noreply.github.com>
2023-08-15 18:57:40 +0500

    Task 1 is completed

--- Vazifa1/Vazifa1/Program.cs
+++ Vazifa1/Vazifa1/Program.cs
@@ -4,7 +4,61 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+        }
+    }
+
+    class Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle()
+        {
+            Width = 0;
+            Height = 0;
+        }
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public void CalculatePerimeter()
+        {
+            Console.WriteLine((Width + Height) * 2);
+        }
+
+        public void CalculateArea()
+        {
+            Console.WriteLine(Width * Height);
+        }
+        public void CalculateDiagonal()
+        {
+            Console.WriteLine(Math.Sqrt((Math.Pow(Width,2) + Math.Pow(Height, 2))));
+        }
+    }
+
+    class Circle
+    {
+        public double Radius { get; set; }
+
+        public Circle()
+        {
+            Radius = 0;
+        }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public void CalculatePerimeter()
+        {
+            Console.WriteLine( 2 * Math.PI * Radius);
+        }
+        public void CalculateArea()
+        {
+            Console.WriteLine( Math.PI * Math.Pow(Radius, 2));
         }
     }
 }
