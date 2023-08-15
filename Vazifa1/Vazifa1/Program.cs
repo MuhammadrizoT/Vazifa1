a2aac577f6fe3b36d1db00ef6cc951e116586c99
TMuhammadrizo <142306600+TMuhammadrizo@users.noreply.github.com>
2023-08-15 19:25:07 +0500

    Task 2 completed
    Added Person, Student, Employe and Teacher classes.
    Person class is parent class with Id, FullName and PhoneNumber proporties.
    Student class inheritance from Person.................................

--- Vazifa1/Vazifa1/Program.cs
+++ Vazifa1/Vazifa1/Program.cs
@@ -61,4 +61,114 @@
             Console.WriteLine( Math.PI * Math.Pow(Radius, 2));
         }
     }
+
+    class Person
+    { 
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Person()
+        {
+            Id = 0;
+            FullName = "";
+            PhoneNumber = "";
+        }
+
+        public Person(int id, string fullname, string phoneNumber)
+        {
+            Id = id;
+            FullName = fullname;
+            PhoneNumber = phoneNumber;
+        }
+    }
+
+    class Student : Person
+    {
+        public double AverageGrade { get; set; }
+        public int[] MarksArray { get; set; }
+
+        public Student()
+        {
+            Id = 0;
+            FullName = string.Empty;
+            PhoneNumber = string.Empty;
+            AverageGrade = 0;
+        }
+
+        public Student(int id, string fullname, string phoneNumber, double averageGrade, int[] marksArray)
+            : base(id, fullname, phoneNumber)
+        {
+            AverageGrade = averageGrade;
+            MarksArray = marksArray;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Id: {Id}\nFullname: {FullName}\nPhoneNumber: {PhoneNumber}\nAverageGrade: {AverageGrade}");
+            Console.WriteLine();
+            for(int i = 0; i < MarksArray.Length; i++)
+            {
+                Console.Write($"{i}. {MarksArray[i]}  ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Employee : Person
+    {
+        public decimal Salary { get; set; }
+
+        public Employee()
+        {
+            Id = 0;
+            FullName = string.Empty;
+            PhoneNumber = string.Empty;
+        }
+
+        public Employee(int id, string fullname, string phoneNumber, decimal salary)
+            : base(id, fullname, phoneNumber)
+        {
+            Salary = salary;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Id: {Id}\nFullname: {FullName}\nPhoneNumber: {PhoneNumber}\nSalary: {Salary}");
+        }
+
+        public void CalculateTax()
+        {
+            decimal tax = Salary / 100 * 13;
+            Console.WriteLine($"Tax: {tax}");
+        }
+    }
+
+    class Teacher : Person
+    {
+        public decimal HourlyRate { get; set; }
+
+        public Teacher()
+        {
+            Id = 0;
+            FullName = string.Empty;
+            PhoneNumber = string.Empty;
+        }
+
+        public Teacher(int id, string fullname, string phoneNumber, decimal hourlyRate)
+            : base(id, fullname, phoneNumber)
+        {
+            HourlyRate = hourlyRate; ;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Id: {Id}\nFullname: {FullName}\nPhoneNumber: {PhoneNumber}\nHourly rate: {HourlyRate}");
+        }
+
+        public void CalculateMontlyIncome(int hours)
+        {
+            Console.WriteLine($"The teacher monthly income: {HourlyRate * hours}");
+        }
+    }
 }
